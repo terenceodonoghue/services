3b0f6c4c75786f05cd2d22a002afacb74708ae57
Terence O'Donoghue <1697905+terenceodonoghue@users.noreply.github.com>
2022-05-18 10:13:52 +1000

    Add env variables, wire up resolver to solar inverter API

--- apps/green-graphql/src/index.ts
+++ apps/green-graphql/src/index.ts
@@ -1,6 +1,10 @@
+import * as dotenv from 'dotenv-safe';
 import 'reflect-metadata';
 import { ApolloServer } from 'apollo-server';
+import axios from 'axios';
 import { buildSchema, Field, ObjectType, Query, Resolver } from 'type-graphql';
+
+dotenv.config();
 
 @ObjectType()
 class PAC {
@@ -35,11 +39,62 @@ class SolarInverter {
   total: Energy;
 }
 
+const instance = axios.create({
+  baseURL: process.env.SOLAR_INVERTER_API_URL,
+  params: {
+    Scope: 'System',
+  },
+  timeout: 1000,
+});
+
+interface Measurable {
+  Unit: string;
+  Values: {
+    1: number;
+  };
+}
+
+interface Response {
+  Body: {
+    Data: {
+      PAC: Measurable;
+      DAY_ENERGY: Measurable;
+      YEAR_ENERGY: Measurable;
+      TOTAL_ENERGY: Measurable;
+    };
+  };
+}
+
 @Resolver()
 class SolarInverterResolver {
-  @Query((returns) => Boolean)
-  async solarInverter() {
-    return true;
+  @Query((returns) => SolarInverter)
+  async solarInverter(): Promise<SolarInverter> {
+    const {
+      data: {
+        Body: {
+          Data: { PAC, DAY_ENERGY, YEAR_ENERGY, TOTAL_ENERGY },
+        },
+      },
+    } = await instance.get<Response>('/GetInverterRealtimeData.cgi');
+
+    return {
+      PAC: {
+        W: PAC.Values[1],
+        kW: Number.parseFloat((PAC.Values[1] / 1000).toFixed(2)),
+      },
+      day: {
+        Wh: DAY_ENERGY.Values[1],
+        kWh: Number.parseFloat((DAY_ENERGY.Values[1] / 1000).toFixed(2)),
+      },
+      year: {
+        Wh: YEAR_ENERGY.Values[1],
+        kWh: Number.parseFloat((YEAR_ENERGY.Values[1] / 1000).toFixed(2)),
+      },
+      total: {
+        Wh: TOTAL_ENERGY.Values[1],
+        kWh: Number.parseFloat((TOTAL_ENERGY.Values[1] / 1000).toFixed(2)),
+      },
+    };
   }
 }
 
